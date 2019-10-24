6cdc58bec7ff2f1f246c7f75da69b25d0ab74e65
weiminxu <weiminxu1982@gmail.com>
2019-10-25 11:50:50 +1300

    add coding step and commence
    add coding step and commence

--- Socket_C++/main.cpp
+++ Socket_C++/main.cpp
@@ -2,4 +2,13 @@
 
 void main() 
 {
+	//Initialze winsock
+	//Create a socket
+	//Bind the socket to an ip address and port
+	//Tell Winsock the socket is for listening
+	//Wait for a connection
+	//Close listening socket
+	//While loop:accept and echo message back to client
+	//Close the sock
+	//Shutdown winsock
 }
