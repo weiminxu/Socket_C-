79c9260125c3ab73f6d8bc360a3c0f66425939ef
weiminxu <weiminxu1982@gmail.com>
2019-10-26 22:24:55 +1300

    While loop:accept and echo message back to client
    While loop:accept and echo message back to client

--- Socket_C++/main.cpp
+++ Socket_C++/main.cpp
@@ -64,6 +64,29 @@ void main()
 	closesocket(listening);
 
 	//While loop:accept and echo message back to client
+	char buf[4096];
+
+	while (true)
+	{
+		ZeroMemory(buf, 4096);
+
+		//Wait for client to send data
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+		if (bytesReceived == SOCKET_ERROR)
+		{
+			cerr << "Error in recv(). Quitting" << endl;
+			break;
+		}
+
+		if (bytesReceived == 0)
+		{
+			cout << "Client disconnected" << endl;
+			break;
+		}
+
+		//Echo message back to client
+		send(clientSocket, buf, bytesReceived + 1, 0);
+	}
 
 	//Close the sock
 
